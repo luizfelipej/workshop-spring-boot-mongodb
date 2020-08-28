a9606a62c69a5c349e6a1a71047f1b6f09c7e2b4
luizfelipej <luizfelipejesus94@gmail.com>
2020-08-28 09:39:29 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/educandoweb/workshopmongo/config/Instantiation.java
+++ src/main/java/com/educandoweb/workshopmongo/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.educandoweb.workshopmongo.config;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TimeZone;
 
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import com.educandoweb.workshopmongo.domain.Post;
 import com.educandoweb.workshopmongo.domain.User;
 import com.educandoweb.workshopmongo.dto.AuthorDTO;
+import com.educandoweb.workshopmongo.dto.CommentDTO;
 import com.educandoweb.workshopmongo.repository.PostRepository;
 import com.educandoweb.workshopmongo.repository.UserRepository;
 
@@ -41,10 +43,17 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "trapezera buscando caraio!",
 				"sabe o que é isso? trapézio descendente! é ele que a gente quer, é ele que a gente vai buscar!",
 				new AuthorDTO(franco));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "BORAA HORA DO SHOW PORRA",
-				"ó o homi ali porra, é 13 porra!", new AuthorDTO(banban));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "BORAA HORA DO SHOW PORR*",
+				"ó o homi ali porr*, é 13 porr*!", new AuthorDTO(banban));
 		Post post3 = new Post(null, sdf.parse("23/03/2018"), "HUUUUUUUUUUUUUUUUUURRRRRR", "QUERO MAIS, QUERO MAIS!",
 				new AuthorDTO(banban));
+
+		CommentDTO c1 = new CommentDTO("é ele né monstro?", sdf.parse("21/03/2018"), new AuthorDTO(banban));
+		CommentDTO c2 = new CommentDTO("aqui nós constroi fibra carai! não é agua com musculo", sdf.parse("23/03/2018"),
+				new AuthorDTO(franco));
+
+		post1.getComments().addAll(Arrays.asList(c1));
+		post2.getComments().addAll(Arrays.asList(c2));
 
 		postRepository.saveAll(Arrays.asList(post1, post2, post3));
 
