e4d0e1ae7ed7f3fb97220a642a3f6a5a2ac1675c
luizfelipej <luizfelipejesus94@gmail.com>
2020-08-28 08:46:44 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/educandoweb/workshopmongo/config/Intantiation.java
+++ src/main/java/com/educandoweb/workshopmongo/config/Intantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.educandoweb.workshopmongo.domain.Post;
 import com.educandoweb.workshopmongo.domain.User;
+import com.educandoweb.workshopmongo.dto.AuthorDTO;
 import com.educandoweb.workshopmongo.repository.PostRepository;
 import com.educandoweb.workshopmongo.repository.UserRepository;
 
@@ -35,13 +36,14 @@ public class Intantiation implements CommandLineRunner {
 		User banban = new User(null, "Banban", "monstro@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+		userRepository.saveAll(Arrays.asList(franco, banban, bob));
+
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "trapezera buscando caraio!",
 				"sabe o que é isso? trapézio descendente! é ele que a gente quer, é ele que a gente vai buscar!",
-				franco);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "BORAA HORA DO SHOW PORRA", "ó o homi ali porra, é 13 porra!",
-				banban);
+				new AuthorDTO(franco));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "BORAA HORA DO SHOW PORRA",
+				"ó o homi ali porra, é 13 porra!", new AuthorDTO(banban));
 
-		userRepository.saveAll(Arrays.asList(franco, banban, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
